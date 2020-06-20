edc405b2b24555e6cec51336febe8aa822b3c573
kuroneko96 <kaityou5555@gmail.com>
2020-06-20 23:33:31 +0900

    [add]Title.hとTitle.cpp

---
+++ Source/Title.cpp
@@ -0,0 +1,26 @@
+#include "Title.h"
+
+//コンストラクタ
+Title::Title() {
+
+}
+
+//初期化処理
+void Title::Init() {
+
+}
+
+//終了処理
+void Title::Final() {
+
+}
+
+//更新処理
+void Title::Update() {
+
+}
+
+//描画処理
+void Title::Draw() {
+
+}
