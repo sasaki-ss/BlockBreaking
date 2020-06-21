609911e7a0bda2c75a97549082580e7a0945ae37
kuroneko96 <kaityou5555@gmail.com>
2020-06-22 00:22:58 +0900

    [add]Result.cppとResult.hを追加

---
+++ Source/Result.cpp
@@ -0,0 +1,26 @@
+#include "Result.h"
+
+//Resultクラスのコンストラクタ
+Result::Result(SceneChanger* changer) :BaseScene(changer) {
+
+}
+
+//初期化処理
+void Result::Init() {
+
+}
+
+//終了処理
+void Result::Final() {
+
+}
+
+//更新処理
+void Result::Update() {
+
+}
+
+//描画処理
+void Result::Draw() {
+
+}
