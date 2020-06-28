fdcab62edff1bcea5a72a944ed03dd15b1d443be
kuroneko96 <kaityou5555@gmail.com>
2020-06-29 00:22:58 +0900

    [docs]コメント整備

--- Source/Title.h
+++ Source/Title.h
@@ -1,20 +1,30 @@
+/**********************************************************************
+ * \file   Title.h
+ * \brief  Titleクラスの宣言
+ * \author かいちょー
+ * \date   2020/6/20
+ * \note   参考サイト⇒https://dixq.net/g/
+ * \par    History
+ *         2020/6/20
+ *         ◆ファイルの作成
+ *********************************************************************/
+
 #ifndef OBJ_TITLE_H
 #define OBJ_TITLE_H
 
 #include "BaseScene.h"
 
-/*
-*	タイトルクラス
-*	タイトルシーン
-*/
-
+ /**********************************************************************
+  * \enum    Title
+  * \brief   タイトルシーン
+  *********************************************************************/
 class Title :public BaseScene {
 public:
-	Title(SceneChanger* changer);	//Titleのコンストラクタ
-	void Init();					//初期化処理
-	void Final();					//終了処理
-	void Update();					//更新処理
-	void Draw();					//描画処理
+	Title(SceneChanger* changer);
+	void Init();
+	void Final();
+	void Update();
+	void Draw();
 };
 
 #endif // !OBJ_TITLE_H
